a3d3d29ddde163cf2947cf1e922719c9405f4e1a
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2022-01-31 03:35:33 +0100

    Add PciIds as encapsulation and a few more functions

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,15 +1,42 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use pci_id::{parse_pci_id_list, DEFAULT_PATH_TO_PCI_IDS};
+use pci_id::{PciIds, DEFAULT_PATH_TO_PCI_IDS};
+use std::path::Path;
 
-pub fn bench_parse_list(c: &mut Criterion) {
-    c.bench_function("parse list 100", |b| {
-        b.iter(|| parse_pci_id_list(DEFAULT_PATH_TO_PCI_IDS))
+pub fn bench_parse_all(c: &mut Criterion) {
+    c.bench_function("parse all", |b| {
+        b.iter(|| {
+            let mut pci_ids = PciIds::new();
+            pci_ids
+                .parse_pci_id_list(Path::new(DEFAULT_PATH_TO_PCI_IDS))
+                .unwrap();
+        })
     });
 }
 
+pub fn bench_parse_vendors(c: &mut Criterion) {
+    c.bench_function("parse only vendors", |b| {
+        b.iter(|| {
+            let mut pci_ids = PciIds::new();
+            pci_ids
+                .parse_vendors(Path::new(DEFAULT_PATH_TO_PCI_IDS))
+                .unwrap();
+        })
+    });
+}
+
+pub fn bench_parse_classes(c: &mut Criterion) {
+    c.bench_function("parse only classes", |b| {
+        b.iter(|| {
+            let mut pci_ids = PciIds::new();
+            pci_ids
+                .parse_classes(Path::new(DEFAULT_PATH_TO_PCI_IDS))
+                .unwrap();
+        })
+    });
+}
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = bench_parse_list
+    targets = bench_parse_all, bench_parse_vendors, bench_parse_classes
 }
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,204 @@
-use std::{io::Error, path::Path};
+use std::num::ParseIntError;
+use std::{io, path::Path};
 
 pub const DEFAULT_PATH_TO_PCI_IDS: &str = "/usr/share/hwdata/pci.ids";
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct PciIds {
+    vendors: Vec<Vendor>,
+    classes: Vec<DeviceClass>,
+}
+
+impl PciIds {
+    pub fn new() -> Self {
+        Self {
+            vendors: Vec::new(),
+            classes: Vec::new(),
+        }
+    }
+
+    pub fn vendors(&self) -> &Vec<Vendor> {
+        &self.vendors
+    }
+
+    pub fn classes(&self) -> &Vec<DeviceClass> {
+        &self.classes
+    }
+
+    pub fn parse_vendors(&mut self, path: &Path) -> Result<(), io::Error> {
+        let data = std::fs::read_to_string(path)?;
+        self.parse_lines(data, false, true).unwrap();
+        Ok(())
+    }
+
+    pub fn parse_classes(&mut self, path: &Path) -> Result<(), io::Error> {
+        let data = std::fs::read_to_string(path)?;
+        self.parse_lines(data, true, false).unwrap();
+        Ok(())
+    }
+
+    pub fn get_vendor(_: &str) -> Result<Vendor, io::Error> {
+        todo!()
+    }
+
+    pub fn get_device(_: &str) -> Result<Device, io::Error> {
+        todo!()
+    }
+
+    pub fn get_subdevice(_: &str) -> Result<SubDevice, io::Error> {
+        todo!()
+    }
+
+    pub fn get_class(_: &str) -> Result<DeviceClass, io::Error> {
+        todo!()
+    }
+
+    pub fn get_subclass(_: &str) -> Result<SubDeviceClass, io::Error> {
+        todo!()
+    }
+
+    pub fn get_programming_interface(_: &str) -> Result<ProgrammingInterface, io::Error> {
+        todo!()
+    }
+
+    #[inline(always)]
+    fn parse_lines(
+        &mut self,
+        data: String,
+        skip_vendors: bool,
+        skip_classes: bool,
+    ) -> Result<(), ParseIntError> {
+        let mut in_class_section = false;
+        let mut vendor: Vendor;
+        let mut device: Device;
+        let mut class: DeviceClass;
+        let mut subclass: SubDeviceClass;
+
+        let mut devices = Vec::new();
+        let mut subdevices = Vec::new();
+        let mut subclasses = Vec::new();
+        let mut interfaces = Vec::new();
+
+        // TODO:Split up list mutation into an inlined function
+        for line in data.lines() {
+            // Skip comments and empty lines
+            if line.starts_with('#') || line.is_empty() {
+                continue;
+            }
+
+            // Should be safe since we check if the line is empty thus the next char is guaranteed
+            // to be there
+            let mut chars = line.chars();
+            let char = chars.next().unwrap();
+
+            let (id, name) = line.split_once("  ").unwrap();
+            let name = name.trim();
+
+            // Line starts with a digit
+            if !skip_vendors && char.is_digit(16) && char != 'C' && !in_class_section {
+                let id = u16::from_str_radix(id.trim(), 16)?;
+                if let Some(v) = self.vendors.last_mut() {
+                    v.set_devices(devices);
+                }
+                vendor = Vendor::new(id, name.to_owned());
+                self.vendors.push(vendor);
+                devices = Vec::new();
+            } else if !skip_vendors && char == '\t' && !in_class_section {
+                // One tab
+                if chars.next().unwrap() != '\t' {
+                    let id = u16::from_str_radix(id.trim(), 16)?;
+                    if let Some(d) = devices.last_mut() {
+                        d.set_subdevices(subdevices);
+                    }
+                    device = Device::new(id, name.to_owned());
+                    devices.push(device);
+                    subdevices = Vec::new();
+                // Two tabs
+                } else {
+                    let (subvendor_id, subdevice_id) = id.split_once(" ").unwrap();
+                    let subvendor_id = u16::from_str_radix(subvendor_id.trim(), 16)?;
+                    let subdevice_id = u16::from_str_radix(subdevice_id.trim(), 16)?;
+                    let subdevice = SubDevice::new(subvendor_id, subdevice_id, name.to_owned());
+                    subdevices.push(subdevice);
+                }
+
+            // Line starts with a C meaning we are in the class section
+            } else if char == 'C' {
+                if skip_classes {
+                    break;
+                }
+
+                if !in_class_section {
+                    in_class_section = true;
+                }
+
+                let (_, id) = id.split_once(" ").unwrap();
+                let id = u8::from_str_radix(id.trim(), 16)?;
+                if let Some(c) = self.classes.last_mut() {
+                    c.set_subclasses(subclasses);
+                }
+                class = DeviceClass::new(id, name.to_owned());
+                self.classes.push(class);
+                subclasses = Vec::new();
+
+            // At this point every line should start with a tab, so no need to check for that
+            } else if !skip_classes && in_class_section {
+                let id = u8::from_str_radix(id.trim(), 16)?;
+                // One tab
+                if chars.next().unwrap() != '\t' {
+                    if let Some(s) = subclasses.last_mut() {
+                        s.set_interfaces(interfaces);
+                    }
+                    subclass = SubDeviceClass::new(id, name.to_owned());
+                    subclasses.push(subclass);
+                    interfaces = Vec::new();
+                }
+                // Two tabs
+                else {
+                    let interface = ProgrammingInterface::new(id, name.to_owned());
+                    interfaces.push(interface);
+                }
+            }
+        }
+        // Add in the last ones
+        if let Some(d) = devices.last_mut() {
+            d.set_subdevices(subdevices);
+        };
+        if let Some(v) = self.vendors.last_mut() {
+            v.set_devices(devices);
+        };
+        if let Some(s) = subclasses.last_mut() {
+            s.set_interfaces(interfaces);
+        };
+        if let Some(c) = self.classes.last_mut() {
+            c.set_subclasses(subclasses);
+        };
+        Ok(())
+    }
+
+    pub fn parse_pci_id_list(&mut self, path: &Path) -> Result<(), io::Error> {
+        let data = std::fs::read_to_string(path)?;
+        self.parse_lines(data, false, false).unwrap();
+
+        Ok(())
+    }
+}
+
+impl Default for PciIds {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Vendor {
-    pub id: u16,
-    pub name: String,
-    pub devices: Vec<Device>,
+    id: u16,
+    name: String,
+    devices: Vec<Device>,
 }
 
 impl Vendor {
-    fn new(id: u16, name: String) -> Self {
+    pub fn new(id: u16, name: String) -> Self {
         Self {
             id,
             name,
@@ -18,25 +206,49 @@ impl Vendor {
         }
     }
 
-    fn set_devices(&mut self, devices: Vec<Device>) {
+    pub fn id(&self) -> u16 {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn devices(&self) -> &Vec<Device> {
+        &self.devices
+    }
+
+    pub fn set_devices(&mut self, devices: Vec<Device>) {
         self.devices = devices;
     }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Device {
-    pub id: u16,
-    pub name: String,
-    pub subdevices: Vec<SubDevice>,
+    id: u16,
+    name: String,
+    subdevices: Vec<SubDevice>,
 }
 
 impl Device {
-    fn new(id: u16, name: String) -> Self {
+    pub fn new(id: u16, name: String) -> Self {
         Self {
             id,
             name,
             subdevices: Vec::new(),
         }
+    }
+
+    pub fn id(&self) -> u16 {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn subdevices(&self) -> &Vec<SubDevice> {
+        &self.subdevices
     }
 
     fn set_subdevices(&mut self, subdevices: Vec<SubDevice>) {
@@ -46,35 +258,59 @@ impl Device {
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct SubDevice {
-    pub subvendor_id: u16,
-    pub subdevice_id: u16,
-    pub name: String,
+    subvendor_id: u16,
+    subdevice_id: u16,
+    name: String,
 }
 
 impl SubDevice {
-    fn new(subvendor_id: u16, subdevice_id: u16, name: String) -> Self {
+    pub fn new(subvendor_id: u16, subdevice_id: u16, name: String) -> Self {
         Self {
             subvendor_id,
             subdevice_id,
             name,
         }
     }
+
+    pub fn subvendor_id(&self) -> u16 {
+        self.subvendor_id
+    }
+
+    pub fn subdevice_id(&self) -> u16 {
+        self.subdevice_id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct DeviceClass {
-    pub id: u8,
-    pub name: String,
-    pub subclasses: Vec<SubDeviceClass>,
+    id: u8,
+    name: String,
+    subclasses: Vec<SubDeviceClass>,
 }
 
 impl DeviceClass {
-    fn new(id: u8, name: String) -> Self {
+    pub fn new(id: u8, name: String) -> Self {
         Self {
             id,
             name,
             subclasses: Vec::new(),
         }
+    }
+
+    pub fn id(&self) -> u8 {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn subclasses(&self) -> &Vec<SubDeviceClass> {
+        &self.subclasses
     }
 
     fn set_subclasses(&mut self, subclasses: Vec<SubDeviceClass>) {
@@ -84,18 +320,30 @@ impl DeviceClass {
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct SubDeviceClass {
-    pub id: u8,
-    pub name: String,
-    pub interfaces: Vec<ProgrammingInterface>,
+    id: u8,
+    name: String,
+    interfaces: Vec<ProgrammingInterface>,
 }
 
 impl SubDeviceClass {
-    fn new(id: u8, name: String) -> Self {
+    pub fn new(id: u8, name: String) -> Self {
         Self {
             id,
             name,
             interfaces: Vec::new(),
         }
+    }
+
+    pub fn id(&self) -> u8 {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn interfaces(&self) -> &Vec<ProgrammingInterface> {
+        &self.interfaces
     }
 
     fn set_interfaces(&mut self, interfaces: Vec<ProgrammingInterface>) {
@@ -105,138 +353,36 @@ impl SubDeviceClass {
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct ProgrammingInterface {
-    pub id: u8,
-    pub name: String,
+    id: u8,
+    name: String,
 }
 
 impl ProgrammingInterface {
-    fn new(id: u8, name: String) -> Self {
+    pub fn new(id: u8, name: String) -> Self {
         Self { id, name }
     }
-}
 
-pub fn parse_pci_id_list<P>(path: P) -> Result<(Vec<Vendor>, Vec<DeviceClass>), Error>
-where
-    P: AsRef<Path>,
-{
-    let mut vendor_list: Vec<Vendor> = Vec::new();
-    let mut class_list: Vec<DeviceClass> = Vec::new();
-    //let mut pci_class_list = Vec::with_capacity(50000);
-    let data = std::fs::read_to_string(path)?;
-
-    let mut in_class_section = false;
-    let mut vendor: Vendor;
-    let mut device: Device;
-    let mut class: DeviceClass;
-    let mut subclass: SubDeviceClass;
-
-    let mut devices = Vec::new();
-    let mut subdevices = Vec::new();
-    let mut subclasses = Vec::new();
-    let mut interfaces = Vec::new();
-
-    // TODO: Need to set the list of things from the last go when a new owner is made
-    for line in data.lines() {
-        // Skip comments and empty lines
-        if line.starts_with('#') || line.is_empty() {
-            continue;
-        }
-
-        // Should be safe since we check if the line is empty
-        let mut chars = line.chars();
-        let char;
-        unsafe {
-            char = chars.next().unwrap_unchecked();
-        }
-
-        let (id, name) = line.split_once("  ").unwrap();
-        let name = name.trim();
-
-        // Line starts with a digit
-        if char.is_digit(16) && char != 'C' && !in_class_section {
-            let id = u16::from_str_radix(id.trim(), 16).unwrap();
-            if let Some(v) = vendor_list.last_mut() {
-                v.set_devices(devices);
-            }
-            vendor = Vendor::new(id, name.to_owned());
-            vendor_list.push(vendor);
-            devices = Vec::new();
-        } else if char == '\t' && !in_class_section {
-            // One tab
-            if chars.next().unwrap() != '\t' {
-                let id = u16::from_str_radix(id.trim(), 16).unwrap();
-                if let Some(d) = devices.last_mut() {
-                    d.set_subdevices(subdevices);
-                }
-                device = Device::new(id, name.to_owned());
-                devices.push(device);
-                subdevices = Vec::new();
-            // Two tabs
-            } else {
-                let (subvendor_id, subdevice_id) = id.split_once(" ").unwrap();
-                let subvendor_id = u16::from_str_radix(subvendor_id.trim(), 16).unwrap();
-                let subdevice_id = u16::from_str_radix(subdevice_id.trim(), 16).unwrap();
-                let subdevice = SubDevice::new(subvendor_id, subdevice_id, name.to_owned());
-                subdevices.push(subdevice);
-            }
-
-        // Line starts with a C or we have entered the device class section at the bottom of the file
-        } else if char == 'C' {
-            if !in_class_section {
-                in_class_section = true;
-            }
-
-            let (_, id) = id.split_once(" ").unwrap();
-            let id = u8::from_str_radix(id.trim(), 16).unwrap();
-            if let Some(c) = class_list.last_mut() {
-                c.set_subclasses(subclasses);
-            }
-            class = DeviceClass::new(id, name.to_owned());
-            class_list.push(class);
-            subclasses = Vec::new();
-
-        // At this point every line should start with a tab, so no need to check for that
-        } else if in_class_section {
-            let id = u8::from_str_radix(id.trim(), 16).unwrap();
-            // One tab
-            if chars.next().unwrap() != '\t' {
-                if let Some(s) = subclasses.last_mut() {
-                    s.set_interfaces(interfaces);
-                }
-                subclass = SubDeviceClass::new(id, name.to_owned());
-                subclasses.push(subclass);
-                interfaces = Vec::new();
-            }
-            // Two tabs
-            else {
-                let interface = ProgrammingInterface::new(id, name.to_owned());
-                interfaces.push(interface);
-            }
-        }
+    pub fn id(&self) -> u8 {
+        self.id
     }
-    // Add in the last ones
-    if let Some(d) = devices.last_mut() {
-        d.set_subdevices(subdevices);
-    };
-    if let Some(v) = vendor_list.last_mut() {
-        v.set_devices(devices);
-    };
-    if let Some(s) = subclasses.last_mut() {
-        s.set_interfaces(interfaces);
-    };
-    if let Some(c) = class_list.last_mut() {
-        c.set_subclasses(subclasses);
-    };
 
-    Ok((vendor_list, class_list))
+    pub fn name(&self) -> &str {
+        &self.name
+    }
 }
 
+#[cfg(test)]
 mod tests {
+    use crate::{PciIds, DEFAULT_PATH_TO_PCI_IDS};
+    use std::path::Path;
     /// Test the vendors part of the parsed result by picking an example and checking if it is ok
     #[test]
     fn test_vendors_list() {
-        let (vendors, _) = crate::parse_pci_id_list(crate::DEFAULT_PATH_TO_PCI_IDS).unwrap();
-        let res = vendors.iter().find(|&v| {
+        let mut pci_ids = PciIds::new();
+        pci_ids
+            .parse_vendors(Path::new(DEFAULT_PATH_TO_PCI_IDS))
+            .unwrap();
+        let res = pci_ids.vendors().iter().find(|&v| {
             v.id == 0x0e11
                 && v.name == "Compaq Computer Corporation"
                 && v.devices
@@ -255,14 +401,17 @@ mod tests {
                     })
                     .is_some()
         });
-        assert!(!res.is_some());
+        assert!(res.is_some());
     }
 
     /// Test the classes part of the parsed result by picking an example and checking if it is ok
     #[test]
     fn test_classes_list() {
-        let (_, classes) = crate::parse_pci_id_list(crate::DEFAULT_PATH_TO_PCI_IDS).unwrap();
-        let res = classes.iter().find(|&c| {
+        let mut pci_ids = PciIds::new();
+        pci_ids
+            .parse_classes(Path::new(DEFAULT_PATH_TO_PCI_IDS))
+            .unwrap();
+        let res = pci_ids.classes().iter().find(|&c| {
             c.id == 0x0c
                 && c.name == "Serial bus controller"
                 && c.subclasses
